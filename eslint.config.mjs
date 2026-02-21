adb0b4d67c4fdf313662a707a648e1c7f17724d1
Ahmedok (Assylzhan Sarinov) <a_sarinov@mail.ru>
2026-02-21 14:15:05 +0000

    feat: Frontend scaffolding (Vite)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
-import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default defineConfig([
     {
@@ -18,5 +19,7 @@ export default defineConfig([
     tseslint.configs.strictTypeChecked,
     tseslint.configs.stylisticTypeChecked,
     pluginReact.configs.flat.recommended,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite,
     eslintPluginPrettierRecommended,
 ]);
